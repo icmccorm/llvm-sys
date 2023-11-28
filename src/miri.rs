097a9e7c1bc5bd8bbb48571072fee03da0fb4854
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-11-28 12:46:45 -0500

    Fixed missing free hook.

--- src/miri.rs
+++ src/miri.rs
@@ -64,6 +64,9 @@ pub type MiriMemcpy = ::std::option::Option<
 pub type MiriAllocationHook = ::std::option::Option<
     unsafe extern "C-unwind" fn(arg1: *mut MiriInterpCxOpaque, arg2: u64, arg3: u64, arg4: bool) -> MiriPointer,
 >;
+pub type MiriFreeHook = ::std::option::Option<
+    unsafe extern "C-unwind" fn(arg1: *mut MiriInterpCxOpaque, arg2: MiriPointer) -> bool,
+>;
 
 pub type MiriGetElementPointerHook = ::std::option::Option<
 unsafe extern "C-unwind" fn(arg1: *mut MiriInterpCxOpaque, arg2: MiriPointer, arg3: u64) -> MiriPointer,
